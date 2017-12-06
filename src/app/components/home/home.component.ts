e313fc7148b2d67005b40a87c78e89963a67cb82
Nishchith J Bangar <nishi.bangar@gmail.com>
2017-12-06 09:49:37 +0530

    Parallax designing and application contents and information complete. Need further improvisation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -102,63 +102,63 @@ export class HomeComponent implements OnInit {
   			title : "Celebrate Music",
   			text : "This year’s music festivals will go down in history.",
   			image : "assets/images/LetsMakeHistoryTogether.jpg"
-  		 }//,
-  		// {
+  		 },
+  		 {
 
-  		// 	title : "Travel back in time and Relive the Madness",
-  		// 	text : " Hundreds and thousands of ravers converge every year in diferent parts of the world to celebrate music ",
-  		// 	image : "assets/images/maxresdefault.jpg"
-  		// },
-  		// {
+  		 	title : "Travel back in time and Relive the Madness",
+  		 	text : " Hundreds and thousands of ravers converge every year in diferent parts of the world to celebrate music ",
+  		 	image : "assets/images/maxresdefault.jpg"
+  		 },
+  		 {
 
-  		// 	title : "Life In Color Festival",
-  		// 	text : "Festival goers are gathering at Miami\'s Sun Life Stadium for the Life In Color festival",
-  		// 	image : "assets/images/ReliveUltraMiami1.jpg"
-  		// },
+  		 	title : "Life In Color Festival",
+  		 	text : "Festival goers are gathering at Miami\'s Sun Life Stadium for the Life In Color festival",
+  		 	image : "assets/images/ReliveUltraMiami1.jpg"
+  		 },
   		
   		
-  		// {
+  		 {
 
-  		// 	title : "The Revival",
-  		// 	text : " The Ultimate Ultra Music Festival Revival ",
-  		// 	image : "assets/images/1502623_10152132683972739_1921784036_o-1.jpg"
-  		// },
-  		// {
+  		 	title : "The Revival",
+  		 	text : " The Ultimate Ultra Music Festival Revival ",
+  		 	image : "assets/images/1502623_10152132683972739_1921784036_o-1.jpg"
+  		 },
+  		 {
 
-  		// 	title : "UNITED WE DANCE",
-  		// 	text : "The festival movement where you lose yourself",
-  		// 	image : "assets/images/12779267_10153457458222078_8158487091733287629_o.jpg"
-  		// },
-  		// {
+  		 	title : "UNITED WE DANCE",
+  		 	text : "The festival movement where you lose yourself",
+  		 	image : "assets/images/12779267_10153457458222078_8158487091733287629_o.jpg"
+  		 },
+  		 {
 
-  		// 	title : "Event of Electronics",
-  		// 	text : "Withness the premier event of electronics",
-  		// 	image : "assets/images/miami-aftermovie-2017-thumb.jpg"
-  		// },
-  		// {
+  		 	title : "Event of Electronics",
+  			text : "Withness the premier event of electronics",
+  		 	image : "assets/images/miami-aftermovie-2017-thumb.jpg"
+  		 },
+  		 {
 
-  		// 	title : "Surprise Set",
-  		// 	text : " Experience Surprise Set From Ultra Music Festival Miami",
-  		// 	image : "assets/images/ReliveUltraMiami2.jpg"
-  		// },
-  		// {
+  		 	title : "Surprise Set",
+  		 	text : " Experience Surprise Set From Ultra Music Festival Miami",
+  		 	image : "assets/images/ReliveUltraMiami2.jpg"
+  		 },
+  		 {
 
-  		// 	title : "The Ultimate Paranoia",
-  		// 	text : "The Ultimate paranoia of music",
-  		// 	image : "assets/images/tomorrowland-2013-aftermovie.jpg"
-  		// },
-  		// {
+  		 	title : "The Ultimate Paranoia",
+  		 	text : "The Ultimate paranoia of music",
+  		 	image : "assets/images/tomorrowland-2013-aftermovie.jpg"
+  		 },
+  		 {
 
-  		// 	title : "UltraMania",
-  		// 	text : "  Ultra is one of the biggest music festivals in the world with 330,000 participants. ",
-  		// 	image : "assets/images/ultra-music-festival-2016-miami.jpg"
-  		// },
-  		// {
+  		 	title : "UltraMania",
+  		 	text : "  Ultra is one of the biggest music festivals in the world with 330,000 participants. ",
+  		 	image : "assets/images/ultra-music-festival-2016-miami.jpg"
+  		 },
+  		 {
 
-  		// 	title : "Be part of the Journey",
-  		// 	text : " Join us on our journey as we relive the unforgettable weekend that was Ultra Music Festival ",
-  		// 	image : "assets/images/Screen-Shot-2016-05-04-at-1.38.59-AM.png"
-  		// },
+  			title : "Be part of the Journey",
+  		 	text : " Join us on our journey as we relive the unforgettable weekend that was Ultra Music Festival ",
+  		 	image : "assets/images/Screen-Shot-2016-05-04-at-1.38.59-AM.png"
+  		 },
   		
 
   	];
